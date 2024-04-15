0463762cabe53223fc3e7cc572914eadbae03e73
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-04-15 12:08:56 +0300

    Add files via upload

---
+++ SarkiListe/FrmMain1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace SarkiListe
+{
+    partial class FrmMain1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvListe = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtSure = new System.Windows.Forms.TextBox();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.chkFavori = new System.Windows.Forms.CheckBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.txtSanatci = new System.Windows.Forms.TextBox();
+            this.txtSarkiAd = new System.Windows.Forms.TextBox();
+            this.txtİd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvListe);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(881, 225);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Şarkı Listesi";
+            // 
+            // dgvListe
+            // 
+            this.dgvListe.AllowUserToAddRows = false;
+            this.dgvListe.AllowUserToDeleteRows = false;
+            this.dgvListe.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvListe.Location = new System.Drawing.Point(3, 22);
+            this.dgvListe.Name = "dgvListe";
+            this.dgvListe.ReadOnly = true;
+            this.dgvListe.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvListe.Size = new System.Drawing.Size(875, 200);
+            this.dgvListe.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtSure);
+            this.groupBox2.Controls.Add(this.dtpTarih);
+            this.groupBox2.Controls.Add(this.cmbTur);
+            this.groupBox2.Controls.Add(this.chkFavori);
+            this.groupBox2.Controls.Add(this.txtYil);
+            this.groupBox2.Controls.Add(this.txtSanatci);
+            this.groupBox2.Controls.Add(this.txtSarkiAd);
+            this.groupBox2.Controls.Add(this.txtİd);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(15, 287);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 312);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Şarkı Bilgileri";
+            // 
+            // txtSure
+            // 
+            this.txtSure.Location = new System.Drawing.Point(131, 174);
+            this.txtSure.Name = "txtSure";
+            this.txtSure.Size = new System.Drawing.Size(185, 26);
+            this.txtSure.TabIndex = 14;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(131, 206);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(248, 26);
+            this.dtpTarih.TabIndex = 13;
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Location = new System.Drawing.Point(131, 238);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(185, 28);
+            this.cmbTur.TabIndex = 12;
+            // 
+            // chkFavori
+            // 
+            this.chkFavori.AutoSize = true;
+            this.chkFavori.Location = new System.Drawing.Point(131, 282);
+            this.chkFavori.Name = "chkFavori";
+            this.chkFavori.Size = new System.Drawing.Size(77, 24);
+            this.chkFavori.TabIndex = 11;
+            this.chkFavori.Text = "Favori";
+            this.chkFavori.UseVisualStyleBackColor = true;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(131, 142);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(185, 26);
+            this.txtYil.TabIndex = 10;
+            // 
+            // txtSanatci
+            // 
+            this.txtSanatci.Location = new System.Drawing.Point(131, 104);
+            this.txtSanatci.Name = "txtSanatci";
+            this.txtSanatci.Size = new System.Drawing.Size(185, 26);
+            this.txtSanatci.TabIndex = 9;
+            // 
+            // txtSarkiAd
+            // 
+            this.txtSarkiAd.Location = new System.Drawing.Point(131, 63);
+            this.txtSarkiAd.Name = "txtSarkiAd";
+            this.txtSarkiAd.Size = new System.Drawing.Size(185, 26);
+            this.txtSarkiAd.TabIndex = 8;
+            // 
+            // txtİd
+            // 
+            this.txtİd.Location = new System.Drawing.Point(131, 25);
+            this.txtİd.Name = "txtİd";
+            this.txtİd.Size = new System.Drawing.Size(185, 26);
+            this.txtİd.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(90, 244);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tür";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(-2, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Eklenme Tarihi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(42, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Süre(DK)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(96, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Yil";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(55, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sanatçı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(44, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şarkı Adı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(97, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnGüncelle);
+            this.groupBox3.Controls.Add(this.btnSil);
+            this.groupBox3.Controls.Add(this.btnEkle);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(427, 287);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(466, 312);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "İşlemler";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(13, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Ara";
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(84, 251);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(809, 20);
+            this.txtAra.TabIndex = 4;
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Image = global::SarkiListe.Properties.Resources._001_arrows;
+            this.btnGüncelle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnGüncelle.Location = new System.Drawing.Point(316, 89);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(144, 101);
+            this.btnGüncelle.TabIndex = 2;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Image = global::SarkiListe.Properties.Resources._003_trash;
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSil.Location = new System.Drawing.Point(166, 89);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(144, 101);
+            this.btnSil.TabIndex = 1;
+            this.btnSil.Text = "Sil";
+            this.btnSil.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Image = global::SarkiListe.Properties.Resources._002_add;
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnEkle.Location = new System.Drawing.Point(7, 89);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(144, 101);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(927, 622);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "FrmMain1";
+            this.Text = "Şarkı Listem";
+            this.Load += new System.EventHandler(this.FrmMain1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvListe;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtSure;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.CheckBox chkFavori;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.TextBox txtSanatci;
+        private System.Windows.Forms.TextBox txtSarkiAd;
+        private System.Windows.Forms.TextBox txtİd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ SarkiListe/FrmMain1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace SarkiListe
+{
+    public partial class FrmMain1 : Form
+    {
+        public FrmMain1()
+        {
+            InitializeComponent();
+        }
+        string baglanti = "Server=localhost;Database=muzik;Uid=root;Pwd=''";
+
+        private void FrmMain1_Load(object sender, EventArgs e)
+        {
+            using(MySqlConnection conn = new MySqlConnection(baglanti))
+            {
+                string sql = "SELECT * FROM sarkilar";
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                dgvListe.DataSource = dt;
+            }
+            
+        }
+    }
+}
